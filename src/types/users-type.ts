d00a4bb7dcfd82681a113213e1a40d4520e31f94
sherkhan61 <65385144+sherkhan61@users.noreply.github.com>
2021-02-08 21:03:26 +0600

    edit actions and reducer of Users components

--- src/types/users-type.ts
+++
@@ -1,9 +0,0 @@
-import {initialState} from '../Redux/reducers/users-reducer'
-import {actions} from '../Redux/actions/users-action'
-import {BaseThunkType, InferActionsTypes} from '../lib/store/root-reducer'
-
-
-export type InitialStateType = typeof initialState
-export type FilterType = typeof initialState.filter
-export type ActionsTypes = InferActionsTypes<typeof actions>
-export type ThunkType = BaseThunkType<ActionsTypes>
