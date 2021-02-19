61f4a48eadf7c10e4df0e692b5f95c946de0c030
sherkhan61 <65385144+sherkhan61@users.noreply.github.com>
2021-02-20 01:04:43 +0600

    Updates

---
+++ precache-manifest.e9320f7818133522a1bd76e9a1286041.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf66e0ca3e193725c6135151cf2c355",
+    "url": "/developers/index.html"
+  },
+  {
+    "revision": "2be5722fc2768e4a1d19",
+    "url": "/developers/static/css/3.7de2207d.chunk.css"
+  },
+  {
+    "revision": "3530702cc1b2c89f40b4",
+    "url": "/developers/static/css/4.5fd8f01f.chunk.css"
+  },
+  {
+    "revision": "da4b927061629079a0a3",
+    "url": "/developers/static/css/5.d28717e2.chunk.css"
+  },
+  {
+    "revision": "7cb14aabe7786d41aa0e",
+    "url": "/developers/static/css/main.b85a7779.chunk.css"
+  },
+  {
+    "revision": "90e7c097dcb02aec0aa3",
+    "url": "/developers/static/js/2.5713e4f0.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/developers/static/js/2.5713e4f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be5722fc2768e4a1d19",
+    "url": "/developers/static/js/3.7b382159.chunk.js"
+  },
+  {
+    "revision": "3530702cc1b2c89f40b4",
+    "url": "/developers/static/js/4.a272964f.chunk.js"
+  },
+  {
+    "revision": "da4b927061629079a0a3",
+    "url": "/developers/static/js/5.d0685cae.chunk.js"
+  },
+  {
+    "revision": "7cb14aabe7786d41aa0e",
+    "url": "/developers/static/js/main.0c78a93a.chunk.js"
+  },
+  {
+    "revision": "d0ba45e7b6a2d3971824",
+    "url": "/developers/static/js/runtime-main.c24ff8ae.js"
+  },
+  {
+    "revision": "34183d97b82ac2556eabdc2dc66d8125",
+    "url": "/developers/static/media/Javascript.34183d97.png"
+  },
+  {
+    "revision": "54c6edcfa1b8312b6f3b6e37ddaf818e",
+    "url": "/developers/static/media/avatar-undefined.54c6edcf.jpg"
+  },
+  {
+    "revision": "0663149eb812dfe291c60b5692af2ab9",
+    "url": "/developers/static/media/entertaiment.0663149e.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/developers/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5cf725f4c5da688d40e4a22be6bb9563",
+    "url": "/developers/static/media/news.5cf725f4.jpg"
+  },
+  {
+    "revision": "cdb7c62a2c0e09eaff8a8c73b74afef4",
+    "url": "/developers/static/media/science.cdb7c62a.jpg"
+  },
+  {
+    "revision": "99fb629b4fc310c263ffa81fb6dcfec1",
+    "url": "/developers/static/media/sport.99fb629b.jpg"
+  },
+  {
+    "revision": "e77eb2d865cfe00b8df167ae46675b10",
+    "url": "/developers/static/media/techology.e77eb2d8.jpg"
+  }
+]);
